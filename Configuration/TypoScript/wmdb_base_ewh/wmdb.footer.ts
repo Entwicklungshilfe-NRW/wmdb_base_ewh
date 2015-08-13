c9686619450096707256501e5a17ae92f1c1268c
Florian Peters <florian.peters@wmdb.de>
2015-08-13 18:23:53 +0200

    WmdbFooter eingebaut

---
+++ Configuration/TypoScript/wmdb_base_ewh/wmdb.footer.ts
@@ -0,0 +1,26 @@
+wmdb.footer = COA
+wmdb.footer {
+    10 = TEXT
+    10.value = WMDB Systems GmbH | Am Wehrhahn 41 | D-40211 Düsseldorf/Germany
+    10.wrap = <p>|</p>
+
+    20 = TEXT
+    20.value = Tel.: +49 (0)211 416 889 20 | Fax: +49 (0)211 416 889 40
+    20.wrap = <p>|</p>
+
+    30 = COA
+    30 {
+        10 = TEXT
+        10.value = Impressum
+        10.typolink.parameter = 12
+
+        20 = TEXT
+        20.value = &nbsp;|&nbsp;
+
+        30 = TEXT
+        30.value = AGB
+        30.typolink.parameter = 11
+
+        wrap = <p>|</p>
+    }
+}
