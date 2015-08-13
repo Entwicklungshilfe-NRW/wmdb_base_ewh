1c59a43f812068583fc5e4b3ccf62af69a5d8400
Florian Peters <florian.peters@wmdb.de>
2015-08-13 17:14:47 +0200

    Content.Ts aus der Framework ersetzt

---
+++ Configuration/TypoScript/wmdb_base_ewh/wmdb.content.ts
@@ -0,0 +1,22 @@
+# Col 0 (Normal column)
+framework.content.0 < styles.content.get
+
+# Col 1 (Left column)
+framework.content.1 < styles.content.get
+framework.content.1.select.where = colPos=1
+
+# Col 2 (Right column)
+framework.content.2 < styles.content.get
+framework.content.2.select.where = colPos=2
+
+# Col 3 (Border column)
+framework.content.3 < styles.content.get
+framework.content.3.select.where = colPos=3
+
+# Col 4
+framework.content.4 < styles.content.get
+framework.content.4.select.where = colPos=4
+
+# Col 5 (Hero Unit)
+framework.content.5 < styles.content.get
+framework.content.5.select.where = colPos=5
