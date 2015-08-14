92cba5352e0309537f5bd886fd702b5a7b68febc
Florian Peters <florian.peters@wmdb.de>
2015-08-14 18:21:07 +0200

    Aenderungen am Footer

--- Configuration/TypoScript/wmdb_base_ewh/wmdb.footer.ts
+++ Configuration/TypoScript/wmdb_base_ewh/wmdb.footer.ts
@@ -1,26 +1,66 @@
 wmdb.footer = COA
 wmdb.footer {
+
     10 = TEXT
-    10.value = WMDB Systems GmbH | Am Wehrhahn 41 | D-40211 Düsseldorf/Germany
-    10.wrap = <p>|</p>
+    10.value (
+        <h4>Know how sources</h4>
+        <ul>
+            <li><a href="https://github.com/Entwicklungshilfe-NRW" target="_blank" data-htmlarea-external="1">Entwicklungshilfe NRW</a></li>
+            <li><a href="https://github.com/wmdbsystems" target="_blank" data-htmlarea-external="1">WMDB Systems</a></li>
+            <li><a href="http://www.rogoit.de/webdesign-typo3-blog-duisburg/" target="_blank" data-htmlarea-external="1">Rogoit Blog</a></li>
+        </ul>
+    )
+    10.wrap = <div class="col-md-3 col-sm-3">|</div>
 
-    20 = TEXT
-    20.value = Tel.: +49 (0)211 416 889 20 | Fax: +49 (0)211 416 889 40
-    20.wrap = <p>|</p>
+    20 = COA
+    20 {
 
-    30 = COA
-    30 {
         10 = TEXT
-        10.value = Impressum
-        10.typolink.parameter = 12
+        10.value (
+            <h4>Anschrift</h4>
+            <ul>
+                <li>WMDB Systems GmbH</li>
+                <li>Am Wehrhahn 41</li>
+                <li>D-40211 Düsseldorf/Germany</li>
+            </ul>
+        )
 
-        20 = TEXT
-        20.value = &nbsp;|&nbsp;
+        20 = COA
+        20 {
 
-        30 = TEXT
-        30.value = AGB
-        30.typolink.parameter = 11
+            10 = TEXT
+            10.value = Impressum
+            10.typolink.parameter = 12
 
-        wrap = <p>|</p>
+            20 = TEXT
+            20.value = &nbsp;|&nbsp;
+
+            30 = TEXT
+            30.value = AGB
+            30.typolink.parameter = 11
+
+        }
+
+        wrap = <div class="col-md-3 col-sm-3">|</div>
     }
+
+    30 = TEXT
+    30.value (
+    )
+    30.wrap = <div class="col-md-3 col-sm-3">|</div>
+
+    40 = TEXT
+    40.value (
+        <ul id="follow_us">
+            <li><a href="https://www.facebook.com/entwicklungshilfe.nrw" target="_blank" data-htmlarea-external="1"><i class="icon-facebook"></i></a></li>
+            <li><a href="https://twitter.com/help_for_devs" target="_blank" data-htmlarea-external="1"><i class="icon-twitter"></i></a></li>
+        </ul>
+        <ul>
+            <li><strong class="phone">+49 (0)211 416 889 20</strong><br /><small>Mon - Fri / 9.00AM - 06.00PM</small></li>
+            <li>Fragen? <a href="mailto:kontakt@entwicklungshilfe.nrw">kontakt@entwicklungshilfe.nrw</a></li>
+        </ul>
+    )
+    40.wrap = <div class="col-md-3 col-sm-3">|</div>
 }
+
+
