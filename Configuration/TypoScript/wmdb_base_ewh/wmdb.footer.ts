d4365e6b9cb3fa7250f0bd0591ba20d2a7522f05
Roland Golla <rolandgolla@gmail.com>
2016-10-03 20:42:49 +0200

    fix footer webvisum telefon

--- Configuration/TypoScript/wmdb_base_ewh/wmdb.footer.ts
+++ Configuration/TypoScript/wmdb_base_ewh/wmdb.footer.ts
@@ -48,9 +48,9 @@ wmdb.footer {
     30.value (
         <h4>Kooperationen</h4>
         <ul>
-            <li><a href="http://www.wmdb.de/" target="_blank" title="TYPO3 Full Service Agentur aus Düsseldorf" data-htmlarea-external="1">WMDB Systems</a></li>
-            <li><a href="http://webvisum.de/" target="_blank" title="Magento und eCommerce Full Service Agentur aus Köln" data-htmlarea-external="1">Webvisum</a></li>
             <li><a href="http://www.rogoit.de/" target="_blank" title="TYPO3 Full Service Agentur aus Duisburg" data-htmlarea-external="1">Rogoit</a></li>
+            <li><a href="http://webvisum.de/" target="_blank" title="Magento und eCommerce Full Service Agentur aus Köln" data-htmlarea-external="1">Webvisum</a></li>
+            <li><a href="http://www.wmdb.de/" target="_blank" title="TYPO3 Full Service Agentur aus Düsseldorf" data-htmlarea-external="1">WMDB Systems</a></li>
         </ul>
     )
     30.wrap = <div class="col-md-3 col-sm-3">|</div>
@@ -63,7 +63,7 @@ wmdb.footer {
             <li><a href="https://twitter.com/help_for_devs" target="_blank" title="PHP-Schulungen Entwicklungshilfe auf Twitter" data-htmlarea-external="1"><i class="icon-twitter"></i></a></li>
         </ul>
         <ul>
-            <li><strong class="phone">+49 (0)211 416 889 20</strong><br /><small>Mon - Fri / 9.00AM - 06.00PM</small></li>
+            <li><strong class="phone">+49 (0)221 677 834 832</strong><br /><small>Mon - Fri / 9.00AM - 06.00PM</small></li>
             <li>Fragen? <a href="mailto:kontakt@entwicklungshilfe.nrw">kontakt@entwicklungshilfe.nrw</a></li>
         </ul>
     )
